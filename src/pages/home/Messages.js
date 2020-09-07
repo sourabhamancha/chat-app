4b984aea2da1c50b5a5e1e903772fd28f5cff717
Sourabh Amancha <sourabh.amancha@gmail.com>
2020-09-07 12:48:29 +0530

    Input for sending messages

--- src/pages/home/Messages.js
+++ src/pages/home/Messages.js
@@ -1,9 +1,9 @@
-import React, { useEffect, Fragment } from "react";
+import React, { useEffect, Fragment, useState } from "react";
 import { useMessageDispatch, useMessageState } from "../../context/message";
 // graphql
-import { gql, useLazyQuery } from "@apollo/client";
+import { gql, useLazyQuery, useMutation } from "@apollo/client";
 // bootstrap
-import { Col } from "react-bootstrap";
+import { Col, Form } from "react-bootstrap";
 // components
 import Message from "./Message";
 
@@ -18,7 +18,20 @@ const GET_MESSAGES = gql`
     }
   }
 `;
+
+const SEND_MESSAGE = gql`
+  mutation sendMessage($to: String!, $content: String!) {
+    sendMessage(input: { to: $to, content: $content }) {
+      uuid
+      content
+      from
+      to
+      createdAt
+    }
+  }
+`;
 function Messages() {
+  const [content, setContent] = useState("");
   const dispatch = useMessageDispatch();
   const { users } = useMessageState();
   const selectedUser = users?.find((u) => u.selected === true);
@@ -27,6 +40,18 @@ function Messages() {
     getMessages,
     { loading: messagesLoading, data: messagesData },
   ] = useLazyQuery(GET_MESSAGES);
+
+  const [sendMessage] = useMutation(SEND_MESSAGE, {
+    onCompleted: (data) =>
+      dispatch({
+        type: "ADD_MESSAGE",
+        payload: {
+          username: selectedUser.username,
+          message: data.sendMessage.content,
+        },
+      }),
+    onError: (err) => console.log(err),
+  });
 
   useEffect(() => {
     if (selectedUser && !selectedUser.messages) {
@@ -45,6 +70,17 @@ function Messages() {
       });
     }
   }, [messagesData]);
+
+  const submitMessage = (e) => {
+    e.preventDefault();
+    if (content === "") return;
+    sendMessage({
+      variables: {
+        to: selectedUser.username,
+        content,
+      },
+    });
+  };
 
   let selectedChatMarkup;
   if (!messages && !messagesLoading) {
@@ -66,8 +102,23 @@ function Messages() {
     selectedChatMarkup = <p>You are now connected! Send your first message</p>;
   }
   return (
-    <Col xs={10} md={8} className="messages-box d-flex flex-column-reverse">
-      {selectedChatMarkup}
+    <Col xs={10} md={8}>
+      <div className="messages-box d-flex flex-column-reverse">
+        {selectedChatMarkup}
+      </div>
+      <div>
+        <Form onSubmit={submitMessage}>
+          <Form.Group>
+            <Form.Control
+              type="text"
+              className="rounded-pill"
+              placeholder="Type your message..."
+              value={content}
+              onChange={(e) => setContent(e.target.value)}
+            />
+          </Form.Group>
+        </Form>
+      </div>
     </Col>
   );
 }
