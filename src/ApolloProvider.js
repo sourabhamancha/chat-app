db4414ab79d1d9d40e9c435c3d5cf8b52baaa940
Sourabh Amancha <sourabh.amancha@gmail.com>
2020-09-10 16:06:57 +0530

    configures apollo for subscriptions

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -5,10 +5,14 @@ import {
   HttpLink,
   ApolloLink,
   ApolloProvider,
+  split,
 } from "@apollo/client";
+import { WebSocketLink } from "@apollo/client/link/ws";
+import { getMainDefinition } from "@apollo/client/utilities";
+
 import App from "./App";
 
-const httpLink = new HttpLink({
+let httpLink = new HttpLink({
   uri: "http://localhost:4000",
 });
 
@@ -27,8 +31,37 @@ const authLink = new ApolloLink((operation, forward) => {
   return forward(operation);
 });
 
+httpLink = authLink.concat(httpLink);
+
+const wsLink = new WebSocketLink({
+  uri: `ws://localhost:4000/graphql`,
+  options: {
+    reconnect: true,
+    connectionParams: {
+      Authorization: `Bearer ${localStorage.getItem("jwtToken")}`,
+    },
+  },
+});
+
+// The split function takes three parameters:
+//
+// * A function that's called for each operation to execute
+// * The Link to use for an operation if the function returns a "truthy" value
+// * The Link to use for an operation if the function returns a "falsy" value
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === "OperationDefinition" &&
+      definition.operation === "subscription"
+    );
+  },
+  wsLink,
+  httpLink
+);
+
 const client = new ApolloClient({
-  link: authLink.concat(httpLink), // Chain it with the HttpLink
+  link: splitLink, // Chain it with the HttpLink
   cache: new InMemoryCache(),
 });
 
